342478a585d462e0b9d9de165ca8b5dec3765859
Rahmat ullah <baheer224@gmail.com>
2022-12-15 09:07:26 +0500

    update the file

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express();
 app.use(express.static("./views"))
 app.set("view engine" , "ejs")
 app.get("/",(req,res)=>{
-     res.render("register")
+     const data =[{name:"Baheer",age:"20"},
+                         {name:"Asalam",age:"60"},
+                         {name:"Rahmat",age:"30"},]
+     res.render("register",{data})
 })
 
 const port = 5000
